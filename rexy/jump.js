8de5269d099bf128e229bf6de3fcee208641d87a
Bobby Marcus <r11marcus11@gmail.com>
2019-05-08 12:59:46 -0400

    fixed issue with touch inputs on mobile devices & cleaned up code

--- rexy/jump.js
+++ rexy/jump.js
@@ -1,254 +1,179 @@
-/*               _       _     _
-                (_)     | |   | |
-__   ____ _ _ __ _  __ _| |__ | | ___  ___
-\ \ / / _` | '__| |/ _` | '_ \| |/ _ \/ __|
- \ V / (_| | |  | | (_| | |_) | |  __/\__ \
-  \_/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/
-*/
-//animations like p5 images should be stored in variables
-//in order to be displayed during the draw cycle
+/*
+_  _ ____ ____ _ ____ ___  _    ____ ____
+|  | |__| |__/ | |__| |__] |    |___ [__
+ \/  |  | |  \ | |  | |__] |___ |___ ___]
+ */
 var rexy;
-// singular sprite/collider to stand on
+var GRAVITY = 1; // how fast rexy falls
+var JUMP = 15; // how high rexy jumps
 var ground;
-// sprite/collider for left side of screen so objects that go off left side get deleted
-var leftSideScreen;
-// create variable to refer to obstacles group
-var obstacles;
-// how often do i want obstacles to spawn?
-var spawnObstacleInterval = 300;	// half second
-var lastSpawnTime;	// keep track of the last time spawn happened
-
+var leftSideScreen; // sprite/collider for left side of screen so objects that go off left side get deleted
+var obstacles; // create variable to refer to obstacles group
+var spawnObstacleInterval = 300;	// how often new obstacles spawn
+var lastSpawnTime;	// keep track of the last osbtacle spawn
+var score = 0; // score counter
+var gameStartedTime = 0; // track how long each game lasts
+var mouseIsClicked = false; // variable to make rexy jump on click/tap
 var gameOver = false;
-var gameOverButton;
-
-// score counter
-var score = 0;
-var gameStartedTime = 0;
+var gameOverButton; // variable for "restart" button
 
 
-/*     _       _           _
-      | |     | |         | |
-  __ _| | ___ | |__   __ _| |
- / _` | |/ _ \| '_ \ / _` | |
-| (_| | | (_) | |_) | (_| | |
- \__, |_|\___/|_.__/ \__,_|_|
-  __/ |
- |___/
+/*
+____ _  _ _  _ ____ ___ _ ____ _  _ ____
+|___ |  | |\ | |     |  | |  | |\ | [__
+|    |__| | \| |___  |  | |__| | \| ___]
 */
-	  // how fast he falls
-var GRAVITY = 1;
-		// how high he jumps
-var JUMP = 15;
+function mousePressed() { // make rexy jump on click/tap
+	mouseisClicked = true;
+	rexy.changeAnimation("jumping");
+	rexy.velocity.y = -JUMP;
+}
+function deleteObstacle(col1, col2) { // delete obstacles that go off left side of screen
+	col2.remove();
+}
+function hitObstacle(collider1, collider2) { // if rexy collides with obstacle -> game over
+	gameOver = true;
+	collider2.velocity.x = 0; // remove obstacle that rexy collided with
+}
+function restartGame() { // reset sketch to start a new game
+	gameOver = false;
+	obstacles.removeSprites();
+	gameOverButton.hide();
+	setup();
+}
 
 
-
-/*              _                 _
-               | |               | |
- _ __  _ __ ___| | ___   __ _  __| |
-| '_ \| '__/ _ \ |/ _ \ / _` |/ _` |
-| |_) | | |  __/ | (_) | (_| | (_| |
-| .__/|_|  \___|_|\___/ \__,_|\__,_|
-| |
-|_|
+/*
+___  ____ ____ _    ____ ____ ___
+|__] |__/ |___ |    |  | |__| |  \
+|    |  \ |___ |___ |__| |  | |__/
 */
 function preload() {
 	// create a rexy sprite
 	rexy = createSprite();
-	// call assets for rexy sprites
 	rexy.addAnimation("running", "img/rexy-pixelated/rexy_00000.png", "img/rexy-pixelated/rexy_00008.png");
 	rexy.addAnimation("jumping", "img/rexy-pixelated/rexyjump.png");
 	rexy.scale = .4;
-	rexy.animation.frameDelay = 4 ;
+	rexy.animation.frameDelay = 4;
 	// load fonts
 	fontMed = loadFont('fonts/BarlowCondensed-Medium.ttf');
 	fontBold = loadFont('fonts/BarlowCondensed-Black.ttf');
 }
 
 
-/*        _
-         | |
- ___  ___| |_ _   _ _ __
-/ __|/ _ \ __| | | | '_ \
-\__ \  __/ |_| |_| | |_) |
-|___/\___|\__|\__,_| .__/
-                   | |
-                   |_|
+/*
+____ ____ ___ _  _ ___
+[__  |___  |  |  | |__]
+___] |___  |  |__| |
 */
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-
-  // button = createImg('');
-  // button.mousePressed(pauseGame);
-
-	// adjust rexy position
-	rexy.position.x = width/5;
-	rexy.position.y = height/2;
-
-	// sprites draw from their center, so start at width/2
-	ground = createSprite(width/2, height - 10, width, height/2);
+	rexy.position.x = width/5; 	// rexy's starting position
+	rexy.position.y = height/2; 	// rexy's starting position
+	ground = createSprite(width/2, height - 10, width, height/2); // (x, y, width. height)
 	ground.shapeColor = 224, 224, 224;
-
-	// tell the sketch that obstacles will be a collection of things
-	obstacles = new Group();
-
-	// if hit left side screen, delete object
-	leftSideScreen = createSprite(0, height/2, 0, height);
-	leftSideScreen.shapeColor = 192, 192, 192;
-
-	// start counting from this moment (for spawns)
-	lastSpawnTime = millis();
-
-	gameStartedTime = millis();
-
-	camera = rexy.position.x;
-
-}
-
-
-/*   _
-    | |
-  __| |_ __ __ ___      __
- / _` | '__/ _` \ \ /\ / /
-| (_| | | | (_| |\ V  V /
- \__,_|_|  \__,_| \_/\_/
-*/
-function draw() {
-	background(204,204,204);
-
-	if(gameOver) {
-		fill(0,0,0);
-		textAlign(CENTER);
-		textFont(fontBold);
-		textSize(100);
-		text("YOU FUCKING SUCK", windowWidth/2, windowHeight/2);
-		textFont(fontMed);
-		textSize(30);
-		text("YOU LASTED " + score + " SECONDS", windowWidth/2, windowHeight/2 + 50);
-
-		if(!gameOverButton){
-		gameOverButton = createButton('TRY AGAIN');
-		}
-	  gameOverButton.style('display', 'block');
-		gameOverButton.mousePressed(newGame);
-
-		if(keyWentDown('space') || mouseWentDown(LEFT))
-			newGame();
-	}
-
-
-	else {
-		// gravity push down!
-		rexy.velocity.y += GRAVITY;
-
-		// did rexy collide with the singular ground object?
-		if (rexy.collide(ground)) {
-			rexy.changeAnimation("running");
-			rexy.velocity.y = 0;
-		}
-
-		// if jump
-		if (keyWentDown("space") || mouseWentDown(LEFT)) {
-			rexy.changeAnimation("jumping");
-			rexy.velocity.y = -JUMP;
-		}
-
-		// spawn obstacles logic
-		if(millis() > lastSpawnTime + spawnObstacleInterval) {
-			// spawn new obstacle
-			var newSprite = createSprite(width, random(height/1.5), 40, 40);
-			// add image to the variable newSprite called "obstacle"
-			newSprite.addAnimation("obstacle", "img/coach.png");
-			// scale "obstacle" image
-			newSprite.scale = 1.5;
-			// set the bounding box
-			newSprite.setCollider("rectangle",0, 0, 30, 20);
-			// give it a negative speed to go from right to left
-			newSprite.velocity.x = -12;
-			// add it to the obstacles group
-			obstacles.add(newSprite);
-			// reset timer
-			lastSpawnTime = millis();
-					// show bounding box for debugging
-					newSprite.debug = false;
-		}
-		// if rexy overlaps with anything in obstacles group, call hitObstacle function
-		rexy.overlap(obstacles, hitObstacle);
-
-		// if obstacles hit left side of screen...
-		leftSideScreen.overlap(obstacles, deleteObstacle);
-
-		// score is how long youre alive
-		score = int((millis() - gameStartedTime) / 1000)
-		// styling for scoreboard text
-		fill(0,0,0);
-		textAlign(LEFT);
-		textFont(fontBold);
-		textSize(30);
-		text("SCORE:", windowWidth/17, 50);
-		textSize(50);
-		text(score, windowWidth/17, 100);
-
-		// needed in p5 play, updates all your sprite objects
-		drawSprites();
-
-	}
-}
-
-
-/*     _         _             _
-      | |       | |           | |
-  ___ | |__  ___| |_ __ _  ___| | ___  ___
- / _ \| '_ \/ __| __/ _` |/ __| |/ _ \/ __|
-| (_) | |_) \__ \ || (_| | (__| |  __/\__ \
- \___/|_.__/|___/\__\__,_|\___|_|\___||___/
-*/
-// delete obstacles that go off left side of screen
-function deleteObstacle(col1, col2) {
-	col2.remove();
-}
-
-// provide this with 2 arguments:
-		// 1: big fish
-		// 2: little fish
-		// in this case, collider1 is rexy, collider2 is obstacle
-function hitObstacle(collider1, collider2) {
-	// game over!
-	gameOver = true;
-
-	// remove thing rexy collided with
-	collider2.velocity.x = 0;
+	leftSideScreen = createSprite(0, height/2, 0, height); // (x, y, width. height)
+	obstacles = new Group(); 	// tell the sketch that obstacles will be a collection of things
+	lastSpawnTime = millis(); // start counting from this moment (FOR OBSTACLE SPAWNS)
+	gameStartedTime = millis(); // start counting from this moment (FOR SCORE COUNTER)
 }
 
 
 /*
- _ __   _____      __   __ _  __ _ _ __ ___   ___
-| '_ \ / _ \ \ /\ / /  / _` |/ _` | '_ ` _ \ / _ \
-| | | |  __/\ V  V /  | (_| | (_| | | | | | |  __/
-|_| |_|\___| \_/\_/    \__, |\__,_|_| |_| |_|\___|
-                        __/ |
-                       |___/  		*/
-// reset sketch to start a new game
-function newGame() {
-	gameOver = false;
-	obstacles.removeSprites();
-	gameOverButton.hide();
-	setup();
-	// updateSprites(true); rexy.position.x = width / 5; rexy.position.y = height-100;
-}
+___  ____ ____ _ _ _
+|  \ |__/ |__| | | |
+|__/ |  \ |  | |_|_|
+*/
+function draw() {
+	background(204,204,204); // background color of sketch canvas
 
-function pauseGame() {
+	/* -+-+-+ +-+-+-+-+
+  |G|A|M|E| |O|V|E|R|
+  +-+-+-+-+ +-+-+-+ */
+	if(gameOver) {
+		if(!gameOverButton){ // create button to restart game
+			gameOverButton = createButton('TRY AGAIN');
+		}
+		  gameOverButton.style('display', 'block');
+			gameOverButton.mousePressed(restartGame);
 
-}
+		fill(0,0,0);
+		textAlign(CENTER);
+		textFont(fontBold);
+		textSize(100);
+		text("YOU LOSE", width/2, height/2);
+		textFont(fontMed);
+		textSize(30);
+		text("YOU LASTED " + score + " SECONDS", width/2, height/2 + 50);
+	} //if(gameOver)
+
+	/* +-+-+-+-+-+-+
+  |P|L|A|Y|I|N|G|
+  +-+-+-+-+-+-+ */
+	else {
+		drawSprites(); // needed for p5.play to work - (updates all your sprite objects)
+
+		score = int((millis() - gameStartedTime) / 1000) // score = how many seconds you're alive
+			fill(0,0,0);
+			textAlign(LEFT);
+			textFont(fontBold);
+			textSize(30);
+			text("SCORE", 30, 50);
+			textSize(50);
+			text(score, 30, 100);
+
+		mouseisClicked = false; // (REFER TO FUNCTION: mousePressed) - makes rexy jump on click/tap
+		rexy.velocity.y += GRAVITY; // use gravity to push down when rexy jumps
+		if (rexy.collide(ground)) { // make rexy run when on the ground
+			rexy.changeAnimation("running");
+			rexy.velocity.y = 0;
+		}
+
+		if(millis() > lastSpawnTime + spawnObstacleInterval) { // logic for spawning obstacles
+			var newSprite = createSprite(width, random(height/1.5), 40, 40); // spawn new obstacle
+			newSprite.addAnimation("obstacle", "img/coach.png"); // add image to the variable newSprite called "obstacle"
+			newSprite.scale = 1.5; 	// scale "obstacle" image
+			newSprite.setCollider("rectangle",0, 0, 30, 20); // set the bounding box of obstacle
+			newSprite.velocity.x = -12; // speed that the obstacles move at
+			obstacles.add(newSprite); // add newSprite to the group "obstacles"
+			lastSpawnTime = millis(); // reset timer every time a new obstacle spawns
+			newSprite.debug = false; // (for debugging) - show bounding box
+		}
+		rexy.overlap(obstacles, hitObstacle); // if rexy overlaps with anything in obstacles group -> call hitObstacle function
+		leftSideScreen.overlap(obstacles, deleteObstacle); // if obstacles hit left side of screen -> call deleteObstacle function
+	} //else
+} //draw
 
 
 
-// gameOverButton.style('position', 'absolute');
-// 	 gameOverButton.style('font-size', '30px');
-// 	 gameOverButton.style('background-color', '#ccc');
-// 	 gameOverButton.style('border', '3px solid #000');
-// 	 gameOverButton.style('box-shadow', '0 0 0 0 rgba(0,0,0,0)');
-// 	 gameOverButton.style('left', '50%');
-// 	 gameOverButton.style('top', '65%');
-// 	 gameOverButton.style('transform', 'translate(-50%, -50%)');
-// 	 gameOverButton.style('font-family', '"BarlowCondensed-Medium"')
-// 	 gameOverButton.style('padding', '1% 4%')
+
+
+
+
+
+
+
+
+/*
+____ ____ _  _ ____ ___     ____ _  _ _ ___  ___  ____ ___ ____
+[__  |__| |  | |___ |  \    [__  |\ | | |__] |__] |___  |  [__
+___] |  |  \/  |___ |__/    ___] | \| | |    |    |___  |  ___]
+*/
+
+// PAUSE BUTTON
+	// put in "setup" function to add pause button
+				// button = createImg('');
+				// button.mousePressed(pauseGame);
+	// put in "functions" setup
+				// function pauseGame() {
+				//
+				// }
+
+// SETTING INPUTS
+	// if (keyWentDown("space")) {
+	// 	rexy.changeAnimation("jumping");
+	// 	rexy.velocity.y = -JUMP;
+	// }
+
+	// if(keyWentDown('space') || mouseWentDown(LEFT))
+	// 	restartGame();
